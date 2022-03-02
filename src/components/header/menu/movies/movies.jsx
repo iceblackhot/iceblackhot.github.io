dc570da436ee2a59211c4f7023ad35b4421313ce
iceblackhot <iceblackhot2@gmail.com>
2022-03-02 15:15:47 +0200

    add_pagination_movies

--- src/components/header/menu/movies/movies.jsx
+++ src/components/header/menu/movies/movies.jsx
@@ -11,24 +11,30 @@ import {motion} from 'framer-motion';
 import {Preloader} from '../../../preloader/preloader';
 import {ButtonUp} from '../buttonUp/buttonUp';
 
+import { Container, Pagination, TextField, Stack, Link } from '@mui/material'
+
 export const Movies = ({movie}) => {
   const [popular, setPopular] = useState([]);
   const [filtered, setFiltered] = useState([]);
   const [activeGenre, setActiveGenre] = useState(0);
 
+    const [page, setPage] = useState(1);
+    const [totalPage, setTotalPage] = useState(20);
+  
   useEffect(() => {
     const fetchPopular = async () => {
+      const BASE_URL = 'https://api.themoviedb.org/3/movie/popular?api_key=3f1fcf681379c251b20684e64bad92df&language=en-US&page='
       const data = await fetch(
-        'https://api.themoviedb.org/3/movie/popular?api_key=3f1fcf681379c251b20684e64bad92df&language=en-US&page=3',
+        BASE_URL + `${page}`
       );
 
       const movies = await data.json();
       setPopular(movies.results);
-      // console.log(movies.results);
+      console.log(movies.results);
       setFiltered(movies.results);
     };
     fetchPopular();
-  }, []);
+  }, [page]);
 
   if (!popular.length) return <Preloader />;
 
@@ -49,6 +55,19 @@ export const Movies = ({movie}) => {
         })}
       </motion.div>
       <ButtonUp />
+      <Container sx={{padding: '15px'}}>
+        <Link />
+        <TextField fullWidth value={page} onChange={(event) => setPage(event.target.value) }/>
+      </Container>
+      <Stack spacing={2}>
+        {!!page && (
+          <Pagination sx={{padding: '15px', marginX:'auto'}}
+          count={totalPage}
+          page={page}
+          onChange={(_, num) =>setPage(num) }
+          />
+        )}
+      </Stack>
     </section>
   );
 };
